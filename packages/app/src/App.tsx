1e30856d4a42cc93664d6325e3186f5fb97d4d6b
Daniel Manser <daniel.manser@awv-informatik.ch>
2023-07-28 00:36:42 +0200

    Integrate Buerligons and add some example stp files

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,16 +1,45 @@
-import "./App.css";
+import React from 'react'
+import './App.css'
 
-import { Buerligons } from "../../buerligons/src/components/Buerligons";
-import { initBuerli } from "../../buerligons/src/initBuerli";
+import { Buerligons } from '../../buerligons/src/components/Buerligons'
+import { initBuerli } from '../../buerligons/src/initBuerli'
 
-initBuerli();
+import { ccAPI } from '@buerli.io/classcad'
+import { DrawingID, api } from '@buerli.io/core'
+
+initBuerli()
 
 function App() {
+  const [drId, setDrId] = React.useState<DrawingID | null>('')
+
+  const load = React.useCallback(
+    (file: string) => {
+      const run = async () => {
+        if (drId) {
+          api.getState().api.removeDrawing(drId)
+        }
+        const id = await ccAPI.base.createCCDrawing()
+        await ccAPI.baseModeler.loadFromUrl(id!, file, 'stp', true)
+        setDrId(id)
+      }
+      run()
+    },
+    [setDrId, drId],
+  )
+
   return (
-    <div style={{ height: "100%", width: "100%" }}>
-      <Buerligons />
+    <div className="main">
+      <div className="header">
+        <a onClick={() => load('http://127.0.0.1:8085/as1_ac_214.stp')}>as1_ac_214.stp</a>
+        <a onClick={() => load('http://127.0.0.1:8085/clamp.stp')}>clamp.stp</a>
+      </div>
+      {drId && (
+        <div className="container">
+          <Buerligons />
+        </div>
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
