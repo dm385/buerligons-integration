5eab90cb6a7aea1f6eb41bc6dab76e04ea932e50
Daniel Manser <daniel.manser@awv-informatik.ch>
2023-07-28 03:03:48 +0200

    Add comment about StrictMode

--- packages/app/src/main.tsx
+++ packages/app/src/main.tsx
@@ -4,6 +4,8 @@ import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
+  // StrictMode renders all components twice in dev mode. So, it's intended to do everything twice!
+  // https://react.dev/reference/react/StrictMode#fixing-bugs-found-by-double-rendering-in-development
   <React.StrictMode>
     <App />
   </React.StrictMode>,
